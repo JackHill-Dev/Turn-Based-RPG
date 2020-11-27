9c08ea7ef99373dbb484dc1311a892439103eed4
Tom Hoare <45071295+Tompoolk@users.noreply.github.com>
2020-11-27 18:17:30 +0000

    SpriteSheet Updated

--- Project(RPG)/Project(RPG)/SpriteSheet.cpp
+++ Project(RPG)/Project(RPG)/SpriteSheet.cpp
@@ -1,5 +1,9 @@
 #include "SpriteSheet.h"
 
+SpriteSheet::SpriteSheet(SDL_Surface* nTexture, float nCellSize, int nCellCount) : mTexture(nTexture), mCellCount(nCellCount), mCellSize(nCellSize)
+{
+}
+
 SDL_Surface* SpriteSheet::GetTexture()
 {
 	return mTexture;
