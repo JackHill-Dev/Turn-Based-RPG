4e05e82e8269133a2e984df86954298c0fc105cc
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-01-05 10:10:11 +0000

    feat: initial Node setup
    Node class in first stage implementation. Need to add on click methods and hook on hover and on leave to trigger on the overworld scene

--- Project(RPG)/Project(RPG)/Node.h
+++ Project(RPG)/Project(RPG)/Node.h
@@ -1,14 +1,37 @@
 #pragma once
-#include "UiObject.h"
+#include "Globals.h"
+#include "RenderObject.h"
 
-
-class Node : public UiObject
+class Node
 {
 private:
 
-	std::string mNodeName;
+	RenderObject* mRenderObj;
+	std::string mNodeName = "";
+	std::string mNodeRole = "";
+	std::pair<int, int> mNodePos = std::make_pair(0, 0);
+	std::vector<Node*> mNodes;
+	
 
 public:
+	Node();
+	~Node();
+	
+	std::string GetNodeName() const;
+	std::string GetNodeRole() const;
+	RenderObject* GetRenderObject();
+	Node* GetNode();
+
+	void setNodeRole(std::string nodeRole);
+	void setNodePos(int xVal, int yVal);
+	void setRenderObject(RenderObject* rObj);
+
+	void OnHover();
+	void OnLeave();
+
+	void AddNode(Node* node);
+	void RemoveNode(Node* node);
+
 
 };
 
