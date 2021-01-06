c6dc1dec0ff678d17f41f38f498917cf4cc3ba30
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-01-06 11:54:24 +0000

    refactor: Mainmenu uses enum for scene change

--- Project(RPG)/Project(RPG)/MainMenuClass.cpp
+++ Project(RPG)/Project(RPG)/MainMenuClass.cpp
@@ -22,7 +22,7 @@ void MainMenuClass::Update(double dTime, Act act, std::pair<int, int> mouse)
 	if (act == Act::Click)
 	{
 		if (start->InBounds(mouse.first, mouse.second))
-			mgr->LoadScene(1);
+			mgr->LoadScene(Scenes::Overworld);
 		else
 			if (quit->InBounds(mouse.first, mouse.second))
 				mgr->Quit();
