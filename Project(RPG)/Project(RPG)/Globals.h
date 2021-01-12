eccd3e4530da564c73f7c84f9519f25362d9eb91
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-01-12 10:05:49 +0000

    refactor: tidy files and update enums
    Removed 'Why' and added NoSceneYet case.

--- Project(RPG)/Project(RPG)/Globals.h
+++ Project(RPG)/Project(RPG)/Globals.h
@@ -5,15 +5,24 @@
 #include <functional>
 
 typedef void (*fnc)(void); // function pointer type
+const int layerNum = 4;
 
 enum Layer
 {
-	UI = 3, Game = 2, Map = 1, Background = 0,
+	 Background = 0,
+	 Map = 1,
+	 Game = 2,
+	 UI = 3, 
 };
 
-const int layerNum = 4;
-
-enum class Act { Blank, Jump, Why, Click, RClick, MouseUpdate };
+enum class Act 
+{ 
+	Blank, 
+	Jump, 
+	Click,
+	RClick,
+	MouseUpdate
+};
 
 enum Scenes
 {
@@ -21,4 +30,5 @@ enum Scenes
 	Overworld = 1,
 	Combat = 2,
 	Shopi = 3,
+	NoSceneYet = 4,
 };
