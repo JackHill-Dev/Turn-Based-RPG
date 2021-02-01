9cc54aba5d05eb64d7020d442987d9e2422bce1c
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-02-01 01:04:27 +0000

    feat: rough implementation
    Needs polishing to remove all the ifs. Currently counts party, adds to it and navigation works

--- Project(RPG)/Project(RPG)/ClassPickerScene.h
+++ Project(RPG)/Project(RPG)/ClassPickerScene.h
@@ -10,12 +10,14 @@ private:
 
 	UIText mHeader;
 	UIText mHeaderInstruction;
-	UIText mWarriorLabel;
-	UIText mRogueLabel;
-	UIText mMageLabel;
+	UIText mFlavourText1;
+	UIText mFlavourText2;
+	UIText mFlavourText3;
 	UIText mFooterInstruction;
 
 	RenderObject* pBackBtn;
+	RenderObject* pRejectBtn;
+	RenderObject* pYesBtn;
 	RenderObject* pWarriorIcon;
 	RenderObject* pRogueIcon;
 	RenderObject* pMageIcon;
@@ -28,13 +30,30 @@ private:
 	// This will be to add to the party - Currently going to get it working with 3.
 	Player* pPlayer;
 
+	bool IsFocused = false;
+	bool IsWarriorView = false;
+	bool IsRogueView = false;
+	bool IsMageView = false;
+
+	int PartyCount = 0;
+
 protected:
 
 public:
 
 	ClassPickerScene(Interface* mObjMgr);
+
 	void Init();
 
+	void OnHover(RenderObject* rObj);
+	void OnLeave(RenderObject* rObj);
+
 	void Update(double dTime, Act act, std::pair<int, int> mousePos) override;
+
+	void SetUpSummaryView();
+	void SetUpWarriorView();
+	void SetUpRogueView();
+	void SetUpMageView();
+
 };
 
