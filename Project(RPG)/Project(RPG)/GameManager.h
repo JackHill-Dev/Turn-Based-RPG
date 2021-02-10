e7abf3034bae24755a4ca6ab5bf4ed4f9ad490f4
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-02-10 16:48:24 +0000

    fix: moved includes across. Added instances back in.
    Keeps .cpp tidy. Allows music to load correctly and for user stats changes and the like to persist.

--- Project(RPG)/Project(RPG)/GameManager.h
+++ Project(RPG)/Project(RPG)/GameManager.h
@@ -14,17 +14,28 @@
 
 #include "Player.h"
 #include "CombatScene.h"
+#include "ClassPickerScene.h"
 #include "Consumable.h"
 #include "Armour.h"
 #include "InventoryScene.h"
+#include "json.hpp"
+#include <fstream>;
+#include <istream>
+
 class GameManager
 {
+
 private:
-	//std::vector<Character*> player{ new Character("maleObj") };
+
 	std::pair<CombatScene*, std::vector<Character*>> combatInstance;
 	PartyViewerScene* partyViewerInstance;
 	InventoryScene* mInventorySceneInstance;
 	ShopScene* mShopSceneInstance;
+	MainMenuScene* mMainMenuSceneInstance;
+	OverworldMapScene* mOverworldInstance;
+	SettingsScene* mSettingsSceneInstance;
+	ClassPickerScene* mClassPickerInstance;
+
 	bool bRunning= true;
 	Scenes mCScene = Scenes::MainMenu;
 	SDL_Window* mWnd;
@@ -35,24 +46,24 @@ private:
 	std::map<std::string, RenderObject*> objects; // This is where the RenderObject types are stored 
 	std::map<std::string, SpriteSheet*> sheets; // This is where the spritesheets are stored
 	std::map<std::string, Card*> cards;
-	//Interface mInterface{&bRunning, &objects, &mPlayer,&mCScene };
-
 
 	Interface mInterface{&bRunning, &objects, &mCScene, &combatInstance.second, &mPlayer, &mSettings};
-
 
 	SDL_Texture* LoadTexture(std::string path); //Moved it here for now
 	void CreateCard(DefinedCard* card);
 	void LoadScene();
 	
 public:
+
 	GameManager() {}
+
 	bool Init();
 
 	void Run();
 	void Quit();	
 	
 private:
+
 	void LoadSettings();
 	
 	bool SetUp();
