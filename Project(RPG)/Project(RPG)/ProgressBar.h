703ac3debce9e6af4ae345734899e8262436999f
Tom Hoare <45071295+Tompoolk@users.noreply.github.com>
2021-02-11 18:25:45 +0000

    A star fixx

--- Project(RPG)/Project(RPG)/ProgressBar.h
+++ Project(RPG)/Project(RPG)/ProgressBar.h
@@ -11,6 +11,7 @@ public:
 	void SetObjects(RenderObject* fill, RenderObject* bg);
 	void SetBarBgObj(RenderObject* bg);
 	void SetBarFillObj(RenderObject* fill);
+	void Scale(std::pair<float, float> scale) { mBarBg->scale = scale; mBarFill->scale = scale; }
 private:
 	float mValue, mMaxValue;
 	float mPercent;
