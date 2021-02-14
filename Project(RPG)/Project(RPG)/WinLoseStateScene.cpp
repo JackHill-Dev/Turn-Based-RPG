ba51fa29de606c276f56ee442abfd2cf9b99b8f9
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-02-14 18:38:02 +0000

    feat: initial set up
    Displays state depending on if any units are alive. Dead units are greyed out. Lose state is finished. Win state needs to be extended to include levelling up. Navigation checks for win state need implementing. Experience table needs implementing.

---
+++ Project(RPG)/Project(RPG)/WinLoseStateScene.cpp
@@ -0,0 +1,221 @@
+#include "WinLoseStateScene.h"
+
+WinLoseStateScene::WinLoseStateScene(Interface* mObjMgr) : Scene(mgr)
+{
+	pBackground = AddObject("scrollBgObj", 640, 360, Map);
+
+	pMenuButton = AddObject("menuButtonObj", 800, 510, UI);
+	pMenuButton->SetVisible(false);
+
+	pQuitButton = AddObject("exitButtonObj", 480, 510, UI);
+	pQuitButton->SetVisible(false);
+
+	Victory_Music = Mix_LoadMUS("Assets/Music/VictoryWindlessSlopes.mp3");
+	Defeat_Music = Mix_LoadMUS("Assets/Music/Game-over-silence.mp3");
+
+	Victory_Sfx = Mix_LoadWAV("Assets/SFX/VictorySfx.wav");
+	Button_Sfx = Mix_LoadWAV("Assets/SFX/GenericClick.wav");
+}
+
+void WinLoseStateScene::Init()
+{
+
+}
+
+void WinLoseStateScene::Load()
+{
+	// Checks if any are alive(You win) or if the party is all dead(Game over) and then calls the relevant starting view,
+	// Win state or lose state 
+	std::vector<Character*> party = mgr->GetPlayer()->GetParty();
+
+	for (auto member : party)
+	{
+		names.push_back(member->GetClassName(member->GetClass()));
+	}
+
+	pLeftSprite = AddObject(party[0]->GetObjName(), 320, 360, UI);
+	pLeftSprite->SetScale(std::make_pair(2, 2));
+
+	pCentreSprite = AddObject(party[1]->GetObjName(), 640, 360, UI);
+	pCentreSprite->SetScale(std::make_pair(2, 2));
+
+	pRightSprite = AddObject(party[2]->GetObjName(), 960, 360, UI);
+	pRightSprite->SetScale(std::make_pair(2, 2));
+
+	if (std::any_of(party.begin(), party.end(), [party](Character* pCharacter) 
+		{
+			return pCharacter->GetDeadStatus() == false;
+		}))
+	{
+		mgr->FadeInMusic(Victory_Music, -1, mgr->fadeTime);
+		SetUpWinState();
+	}
+	else
+	{
+		mgr->FadeInMusic(Defeat_Music, 1, mgr->fadeTime); // This one doesn't loop - EH
+		SetUpLoseState();
+	}
+
+}
+
+void WinLoseStateScene::Update(double dTime, Act act, std::pair<int, int> mousePos)
+{
+
+	if (act == Act::MouseUpdate)
+	{
+		if (pQuitButton->InBounds(mousePos.first, mousePos.second))
+		{
+			OnHover(pQuitButton);
+		}
+
+		if (pMenuButton->InBounds(mousePos.first, mousePos.second))
+		{
+			OnHover(pMenuButton);
+		}
+	}
+
+	if (act == Act::Click)
+	{
+		if (pMenuButton->InBounds(mousePos.first, mousePos.second))
+		{
+			mgr->PlaySFX(Button_Sfx, 0, 1);
+			mgr->LoadScene(Scenes::MainMenu);
+		}
+		if (pQuitButton->InBounds(mousePos.first, mousePos.second))
+		{
+			mgr->PlaySFX(Button_Sfx, 0, 1);
+			mgr->Quit();
+		}
+	}
+}
+
+void WinLoseStateScene::OnHover(RenderObject* rObj)
+{
+	rObj->Tint(Lime);
+}
+
+void WinLoseStateScene::OnLeave(RenderObject* rObj)
+{
+	rObj->Untint();
+}
+
+void WinLoseStateScene::AddExperience()
+{
+}
+
+void WinLoseStateScene::SetUpWinState()
+{
+	std::vector<Character*> party = mgr->GetPlayer()->GetParty();
+	int dividedXp = mgr->GetPlayer()->GetXpPool() / party.size();
+
+	for (auto member : party)
+	{
+		member->modStat(member->GetStats().experience, {dividedXp, 0});
+	}
+
+	mgr->GetPlayer()->ClearXpPool();
+
+	if (party[0]->GetDeadStatus() == true)
+	{
+		pLeftSprite->tint = DimGray;
+	}
+
+	if (party[1]->GetDeadStatus() == true)
+	{
+		pCentreSprite->tint = DimGray;
+	}
+
+	if (party[2]->GetDeadStatus() == true)
+	{
+		pLeftSprite->tint = DimGray;
+	}
+
+	pMenuButton->SetVisible(false);
+	pQuitButton->SetVisible(false);
+
+	mHeader.text = "YOU WIN";
+	mHeader.pos = std::make_pair<int>(640, 230);
+	mHeader.textColor = SDL_Color{ 0,0,0 };
+	mHeader.SetTextScale(300, 60);
+
+	mHeaderInstruction.text = "SPOILS OF WAR";
+	mHeaderInstruction.textColor = SDL_Color{ 0,0,0 };
+	mHeaderInstruction.pos = std::make_pair<int>(640, 270);
+	mHeaderInstruction.SetTextScale(250, 40);
+
+	mFlavourText1.text = names[0];
+	mFlavourText1.textColor = SDL_Color{ 0,0,0 };
+	mFlavourText1.pos = std::make_pair<int>(320, 410);
+	mFlavourText1.SetTextScale(100, 30);
+
+	mFlavourText2.text = names[1];
+	mFlavourText2.textColor = SDL_Color{ 0,0,0 };
+	mFlavourText2.pos = std::make_pair<int>(640, 410);
+	mFlavourText2.SetTextScale(100, 30);
+
+	mFlavourText3.text = names[2];
+	mFlavourText3.textColor = SDL_Color{ 0,0,0 };
+	mFlavourText3.pos = std::make_pair<int>(960, 410);
+	mFlavourText3.SetTextScale(100, 30);
+
+	mFooterInstruction.text = "PLEASE SELECT YOUR CHOICE WITH THE LEFT MOUSE BUTTON";
+	mFooterInstruction.textColor = SDL_Color{ 0,0,0 };
+	mFooterInstruction.pos = std::make_pair<int>(640, 550);
+	mFooterInstruction.SetTextScale(700, 40);
+
+	mSceneText.push_back(&mHeader);
+	mSceneText.push_back(&mHeaderInstruction);
+	mSceneText.push_back(&mFlavourText1);
+	mSceneText.push_back(&mFlavourText2);
+	mSceneText.push_back(&mFlavourText3);
+	mSceneText.push_back(&mFooterInstruction);
+}
+
+void WinLoseStateScene::SetUpLoseState()
+{
+
+	pLeftSprite->tint = DimGray;
+	pCentreSprite->tint = DimGray;
+	pLeftSprite->tint = DimGray;
+	
+	pMenuButton->SetVisible(true);
+	pQuitButton->SetVisible(true);
+
+	mHeader.text = "YOU LOSE";
+	mHeader.pos = std::make_pair<int>(640, 230);
+	mHeader.textColor = SDL_Color{ 0,0,0 };
+	mHeader.SetTextScale(300, 60);
+
+	mHeaderInstruction.text = "TRY AGAIN?";
+	mHeaderInstruction.textColor = SDL_Color{ 0,0,0 };
+	mHeaderInstruction.pos = std::make_pair<int>(640, 270);
+	mHeaderInstruction.SetTextScale(250, 40);
+
+	mFlavourText1.text = names[0];
+	mFlavourText1.textColor = SDL_Color{ 0,0,0 };
+	mFlavourText1.pos = std::make_pair<int>(320, 410);
+	mFlavourText1.SetTextScale(100, 30);
+
+	mFlavourText2.text = names[1];
+	mFlavourText2.textColor = SDL_Color{ 0,0,0 };
+	mFlavourText2.pos = std::make_pair<int>(640, 410);
+	mFlavourText2.SetTextScale(100, 30);
+
+	mFlavourText3.text = names[2];
+	mFlavourText3.textColor = SDL_Color{ 0,0,0 };
+	mFlavourText3.pos = std::make_pair<int>(960, 410);
+	mFlavourText3.SetTextScale(100, 30);
+
+	mFooterInstruction.text = "PLEASE SELECT YOUR CHOICE WITH THE LEFT MOUSE BUTTON";
+	mFooterInstruction.textColor = SDL_Color{ 0,0,0 };
+	mFooterInstruction.pos = std::make_pair<int>(640, 550);
+	mFooterInstruction.SetTextScale(700, 40);
+
+	mSceneText.push_back(&mHeader);
+	mSceneText.push_back(&mHeaderInstruction);
+	mSceneText.push_back(&mFlavourText1);
+	mSceneText.push_back(&mFlavourText2);
+	mSceneText.push_back(&mFlavourText3);
+	mSceneText.push_back(&mFooterInstruction);
+
+}
---
+++ Project(RPG)/Project(RPG)/WinLoseStateScene.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "Scene.h"
+#include "Actions.h"
+#include "Character.h"
+#include <SDL.h>
+
+
+class WinLoseStateScene : public Scene
+{
+private:
+
+	UIText mHeader;
+	UIText mHeaderInstruction;
+	UIText mFlavourText1;
+	UIText mFlavourText2;
+	UIText mFlavourText3;
+	UIText mFlavourText4;
+	UIText mFlavourText5;
+	UIText mFlavourText6;
+	UIText mFooterInstruction;
+
+	RenderObject* pBackground = nullptr;
+	RenderObject* pLevelUpOverlay = nullptr;
+	RenderObject* pConfirmButton = nullptr;
+	RenderObject* pRejectButton = nullptr;
+	RenderObject* pQuitButton = nullptr;
+	RenderObject* pMenuButton = nullptr;
+	RenderObject* pLeftSprite = nullptr;
+	RenderObject* pCentreSprite = nullptr;
+	RenderObject* pRightSprite = nullptr;
+
+	Mix_Music* Victory_Music = nullptr;
+	Mix_Music* Defeat_Music = nullptr;
+
+	Mix_Chunk* Victory_Sfx = nullptr;
+	Mix_Chunk* Defeat_Sfx = nullptr;
+	Mix_Chunk* Button_Sfx = nullptr;
+
+	SDL_Color Lime = SDL_Color{ 0, 255, 0 };
+	SDL_Color DimGray = SDL_Color{ 105,105,105 };
+
+	std::vector<std::string> names;
+	
+
+
+
+protected:
+
+public:
+
+	WinLoseStateScene(Interface* mObjMgr);
+
+	void Init();
+	void Load();
+	void Update(double dTime, Act act, std::pair<int, int> mousePos) override;
+
+	void OnHover(RenderObject* rObj);
+	void OnLeave(RenderObject* rObj);
+
+	void AddExperience();
+	void SetUpWinState();
+	void SetUpLoseState();
+
+};
+
