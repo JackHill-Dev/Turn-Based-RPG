ef20c0a2f75461e2e0fa99c4d7a9eaf30a410d32
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-02-20 15:55:00 +0000

    fix: settings navigation
    Settings correctly uses load previous scene method.

--- Project(RPG)/Project(RPG)/SettingsScene.cpp
+++ Project(RPG)/Project(RPG)/SettingsScene.cpp
@@ -42,7 +42,7 @@ void SettingsScene::Update(double dTime, Act act, std::pair<int, int> mousePos)
 		if (settingsCloseBtn->InBounds(mousePos.first, mousePos.second) && settingsCloseBtn->IsActive())
 		{
 			mgr->PlaySFX(button_Click_SFX, 0, 1);
-			mgr->LoadScene(MainMenu);
+			mgr->LoadPreviousScene();
 			settingsCloseBtn->Untint();
 		}
 
