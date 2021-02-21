78f8e5cb79b14ef40a447fd1d64ee219afc99e1b
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-02-21 19:20:43 +0000

    fix: weapon and armour can only be equipped to correct slot.

--- Project(RPG)/Project(RPG)/InventoryScene.cpp
+++ Project(RPG)/Project(RPG)/InventoryScene.cpp
@@ -109,7 +109,7 @@ void InventoryScene::Update(double dTime, Act act, std::pair<int, int> mousePos)
 						{
 							for (auto& c : characters)
 							{
-								if (c.armourSlot->InBounds(mousePos.first, mousePos.second) && i.bPickedUp && c.character->ArmourEquipSlot == nullptr)
+								if (c.armourSlot->InBounds(mousePos.first, mousePos.second) && i.bPickedUp && c.character->ArmourEquipSlot == nullptr && (dynamic_cast<Armour*>(i._item)))
 								{
 									i.bPickedUp = false;
 									i.obj->SetPos(c.armourSlot->GetPos());
@@ -118,7 +118,7 @@ void InventoryScene::Update(double dTime, Act act, std::pair<int, int> mousePos)
 
 								}
 								else
-									if (c.weaponSlot->InBounds(mousePos.first, mousePos.second) && i.bPickedUp && c.character->mWeaponEquipSlot == nullptr)
+									if (c.weaponSlot->InBounds(mousePos.first, mousePos.second) && i.bPickedUp && c.character->mWeaponEquipSlot == nullptr && (dynamic_cast<Weapon*>(i._item)))
 									{
 										i.bPickedUp = false;
 										i.obj->SetPos(c.weaponSlot->GetPos());
