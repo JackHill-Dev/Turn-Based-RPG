55315ae8e5b001e2e5fa45fa7cb827cbdf4f7ddb
Elliot Hunter <b7026503@my.shu.ac.uk>
2021-03-01 19:27:11 +0000

    refactor: extend arrow range

--- Project(RPG)/Project(RPG)/GameObjects.h
+++ Project(RPG)/Project(RPG)/GameObjects.h
@@ -365,7 +365,7 @@ const std::map<std::string, DefinedCard> definedCards
 {
 	// Name,  Range, damage, costs(STR, INT AGI) , card name, flavour, picture, animation object, animation length
 	{"magicCard",{3, 7, 0, 5, 0, "Magic", "Magically assail the enemy for 7 damage, costs 5 intelligence", "Magic_Card" ,"MagicObj", 0.75}},
-	{"arrowCard", {4, 5, 0, 0, 5, "Shoot", "Pepper your foe with arrows for 5 damage, costs 5 agility", "Shootcard", "ArrowShotObj", 0.75}},
+	{"arrowCard", {5, 5, 0, 0, 5, "Shoot", "Pepper your foe with arrows for 5 damage, costs 5 agility", "Shootcard", "ArrowShotObj", 0.75}},
 	{"slashCard",{1.5, 10, 5, 0, 0, "Slash", "Slash the enemy for 10 damage, costs 5 strength", "SlashCard" ,"swordSlashEffectObj", 0.5}},	
 	{"healCard",{1.5, -10, 0, 10, 0, "Heal", "Heal the target for 15 health, costs 10 intelligence", "HealCard" ,"healImageObject", 1.25}},
 	{"bossCard",{ 1.5, 10, 5, 0, 0, "BossAttack", "The Boss cleaves it's foe for 10 damage, costs 5 strength", "SlashCard", "BossAttackObj", 1}},
