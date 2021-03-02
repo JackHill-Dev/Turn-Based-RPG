929bb3842522aa07ad9ecb982b8652c945ad5478
TomHoare97 <60366420+TomHoare97@users.noreply.github.com>
2021-03-02 15:59:22 +0000

    Boss fixed

--- Project(RPG)/Project(RPG)/BossScene.cpp
+++ Project(RPG)/Project(RPG)/BossScene.cpp
@@ -879,7 +879,13 @@ void BossScene::RunAi()
 
 	if (!validAction)
 	{
+		for (int i = 0; i < enemyHand.size(); ++i)
+		{
+			delete enemyHand[i].first;
 
+		}
+
+		enemyHand.clear();
 		for (int i = 0; i < playerhand.size(); ++i)
 		{
 			mLayers[UI].erase(std::find(mLayers[UI].begin(), mLayers[UI].end(), playerhand[i].second));
